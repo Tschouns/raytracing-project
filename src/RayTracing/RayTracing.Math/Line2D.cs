d54e49b1a8ebac5633bfd40e4c4a261e32bd10f8
Jonas <jonas.aklin@gmail.com>
2023-09-29 12:12:17 +0200

    Removed namespace

---
+++ src/RayTracing/RayTracing.Math/Line2D.cs
@@ -0,0 +1,25 @@
+﻿namespace RayTracing.Math
+{
+    public struct Line2D
+    {
+        public Line2D()
+        {
+            PointA = new Vector2();
+            PointB = new Vector2();
+        }
+
+        public Line2D(Vector2 pointA, Vector2 pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+
+        public Vector2 PointA { get; }
+        public Vector2 PointB { get; }
+
+        public float Length()
+        {
+            return (PointB - PointA).Length();
+        }
+    }
+}
