57ff1be785a04e61e69e9bdb753433ea7dee4a2f
Jonas <jonas.aklin@gmail.com>
2023-10-05 15:21:36 +0200

    WIP

--- src/RayTracing/RayTracing.Math/Calculations/VectorCalculator3D.cs
+++ src/RayTracing/RayTracing.Math/Calculations/VectorCalculator3D.cs
@@ -3,6 +3,23 @@ namespace RayTracing.Math.Calculations
 {
     public static class VectorCalculator3D
     {
-        //public static Vector3? Intersect(Line3D line, )
+        /// <summary>
+        /// Calculates the intersection between a specified line with a specified plane.
+        /// </summary>
+        public static Vector3? Intersect(Line3D line, Plane3D plane)
+        {
+
+        }
+
+        /// <summary>
+        /// Calculates the intersection between a specified line with a specified plane, represented
+        /// as follows:
+        /// Line: r = p + lambda * u
+        /// Plane: r = q + mu * v + nu * w
+        /// </summary>
+        public static Vector3? Intersect(Vector3 p, Vector3 u, Vector3 q, Vector3 v, Vector3 w)
+        {
+
+        }
     }
 }
