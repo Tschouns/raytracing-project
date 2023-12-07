421a273f8beef0eeb2f2b8518da3c408105c6ae8
Jonas <jonas.aklin@gmail.com>
2023-12-07 22:25:29 +0100

    img

--- src/RayTracing/RayTracing.TestBench/ExamplesRendering.cs
+++ src/RayTracing/RayTracing.TestBench/ExamplesRendering.cs
@@ -68,8 +68,8 @@ namespace RayTracing.TestBench
                     bottleMaterial.Glossyness = 0.7f;
                     bottleMaterial.Transparency = 0.5f;
                 },
-                resX: 50,
-                resY: 60,
+                resX: 100,
+                resY: 120,
                 camera =>
                 {
                     camera.Position = new Vector3(0, 1f, -1.3f);
