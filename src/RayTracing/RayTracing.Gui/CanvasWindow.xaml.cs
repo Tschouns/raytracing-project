17fcd1648e0e1b3b0ca00ee488eaa6327c63f4e5
Jonas <jonas.aklin@gmail.com>
2024-01-20 17:45:41 +0100

    Added project

---
+++ src/RayTracing/RayTracing.Gui/CanvasWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RayTracing.Gui
+{
+    /// <summary>
+    /// Interaction logic for CanvasWindow.xaml
+    /// </summary>
+    public partial class CanvasWindow : Window, ICanvas
+    {
+        private WriteableBitmap bitmap;
+
+        public CanvasWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Size(int width, int height)
+        {
+            var newBitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, null);
+            bitmap = newBitmap;
+            image.Source = bitmap;
+        }
+
+        public void Fill(System.Drawing.Color color)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Pixel(int x, int y, System.Drawing.Color color)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/RayTracing/RayTracing.Gui/ICanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace RayTracing.Gui
+{
+    public interface ICanvas
+    {
+        void Size(int width, int height);
+        void Pixel(int x, int y, Color color);
+        void Fill(Color color);
+    }
+}
