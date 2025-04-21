a112c335b6a2d8451a83a9a11b3f3dce835360e0
Jonas <jonas.aklin@gmail.com>
2025-04-21 17:10:28 +0200

    Add Octree (WIP)

---
+++ src/RayTracing/RayTracing.Model/Octrees/Octree.cs
@@ -0,0 +1,27 @@
+﻿using RayTracing.Base;
+using RayTracing.Math;
+
+namespace RayTracing.Model.Octrees
+{
+    public class Octree
+    {
+        public Octree(
+            AxisAlignedBoundingBox boundingBox,
+            IEnumerable<Face> allFaces,
+            IEnumerable<Octree> children)
+        {
+            Argument.AssertNotNull(boundingBox, nameof(boundingBox));
+            Argument.AssertNotNull(allFaces, nameof(allFaces));
+            Argument.AssertNotNull(children, nameof(children));
+
+            this.BoundingBox = boundingBox;
+            this.AllFaces = allFaces;
+            this.Children = children;
+        }
+
+        public AxisAlignedBoundingBox BoundingBox { get; }
+        IEnumerable<Face> AllFaces { get; }
+        public IEnumerable<Octree> Children { get; }
+        public bool HasChildren => this.Children.Any();
+    }
+}
---
+++ src/RayTracing/RayTracing.Model/Octrees/OctreeHelper.cs
@@ -0,0 +1,34 @@
+﻿
+using RayTracing.Base;
+using RayTracing.Math;
+using RayTracing.Math.Calculations;
+
+namespace RayTracing.Model.Octrees
+{
+    public static class OctreeHelper
+    {
+        public static Octree PrepareOctree(IEnumerable<Face> allFaces, int splitTheshold)
+        {
+            Argument.AssertNotNull(allFaces, nameof(allFaces));
+
+            var allVertices = allFaces
+                .SelectMany(f => new[] { f.Triangle.CornerA, f.Triangle.CornerB, f.Triangle.CornerC })
+                .ToList();
+
+            var minX = allVertices.Min(v => v.X);
+            var minY = allVertices.Min(v => v.Y);
+            var minZ = allVertices.Min(v => v.Z);
+
+            var maxX = allVertices.Max(v => v.X);
+            var maxY = allVertices.Max(v => v.Y);
+            var maxZ = allVertices.Max(v => v.Z);
+
+            var min = new Vector3(minX, minY, minZ);
+            var max = new Vector3(maxX, maxY, maxZ);
+            var boundingBox = new AxisAlignedBoundingBox(min, max);
+
+            // TODO: finish implementation.
+            return new Octree(boundingBox, allFaces, new Octree[0]);
+        }
+    }
+}
