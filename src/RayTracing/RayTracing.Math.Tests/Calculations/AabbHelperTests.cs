64efd9492d13dc1e6d05464d1d7547ff233e06c8
Jonas <jonas.aklin@gmail.com>
2025-04-23 11:23:02 +0200

    Extracted AabbHelper, added test

---
+++ src/RayTracing/RayTracing.Math.Tests/Calculations/AabbHelperTests.cs
@@ -0,0 +1,25 @@
+﻿
+using RayTracing.Math.Calculations;
+
+namespace RayTracing.Math.Tests.Calculations
+{
+    public class AabbHelperTests
+    {
+        [Fact]
+        public void PrepareOctants_GivenParentBox_ReturnsEightOctants()
+        {
+            // Arrange
+            var box = new AxisAlignedBoundingBox(
+                min: new Vector3(0, 0, 0),
+                max: new Vector3(2, 4, 6));
+
+            // Act
+            var octants = AabbHelper.PrepareOctants(box);
+
+            // Assert
+            Assert.Equal(8, octants.Count());
+            Assert.Equal(box.Min, octants.First().Min);
+            Assert.Equal(box.Max, octants.Last().Max);
+        }
+    }
+}
--- src/RayTracing/RayTracing.Model/Octrees/OctreeHelper.cs
+++ src/RayTracing/RayTracing.Model/Octrees/OctreeHelper.cs
@@ -29,7 +29,7 @@ namespace RayTracing.Model.Octrees
             var boundingBox = new AxisAlignedBoundingBox(min, max);
 
             var children = new Collection<Octree>();
-            var childBoxes = ChildBoundingBoxes(boundingBox);
+            var childBoxes = AabbHelper.PrepareOctants(boundingBox);
 
             foreach (var childBox in childBoxes)
             {
@@ -42,41 +42,6 @@ namespace RayTracing.Model.Octrees
 
             // TODO: finish implementation.
             return new Octree(boundingBox, allFaces, children);
-        }
-
-        private static IEnumerable<AxisAlignedBoundingBox> ChildBoundingBoxes(AxisAlignedBoundingBox parent)
-        {
-            var min = parent.Min;
-            var max = parent.Max;
-
-            var c = (max - min) / 2;    // a vector, half way from min to max
-            var center = min + c;       // the center of the parent box
-
-            var x = new Vector3(c.X, 0, 0); // the X component of the c vector
-            var y = new Vector3(0, c.Y, 0); // the Y component of the c vector
-            var z = new Vector3(0, 0, c.Z); // the Z component of the c vector
-
-            var box000 = new AxisAlignedBoundingBox(min, center);
-            var box00x = new AxisAlignedBoundingBox(min + x, center + x);
-            var box0y0 = new AxisAlignedBoundingBox(min + y, center + y);
-            var box0yx = new AxisAlignedBoundingBox(min + x + y, center + x + y);
-
-            var boxZ00 = new AxisAlignedBoundingBox(center - x - y, max - x - y);
-            var boxZ0x = new AxisAlignedBoundingBox(center - y, max - y);
-            var boxZy0 = new AxisAlignedBoundingBox(center - x, max - x);
-            var boxZyx = new AxisAlignedBoundingBox(center, max);
-
-            return new[]
-            {
-                box000,
-                box00x,
-                box0y0,
-                box0yx,
-                boxZ00,
-                boxZ0x,
-                boxZy0,
-                boxZyx,
-            };
         }
 
         private static bool DoesTriangleIntersectBox(Triangle3D triangle, AxisAlignedBoundingBox box)
